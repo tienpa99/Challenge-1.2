4928036a201c9bf57538d10bbfcc7909d42944ff
TIENPHAM1 <tienpham1@1-TIENPHAM1-M.local>
2023-04-01 20:03:13 +0700

    update secret

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php $api_key = 'https://maps.googleapis.com/maps/api/staticmap?center=45,10&zoom=7&size=400x400&key=AIzaSyDTb7FjBOTtRDInWkPyCgMCbkJ5PrLoE_8'
+
+
+https://maps.googleapis.com/maps/api/staticmap?center=45,10&zoom=7&size=400x400&key=AIzaSyDTb7FjBOTtRDInWkPyCgMCbkJ5PrLoE_8
+
+google_api_key = AIzaSyDTb7FjBOTtRDInWkPyCgMCbkJ5PrLoE_8
